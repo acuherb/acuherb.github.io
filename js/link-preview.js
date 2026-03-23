b2044a43cbd589ed477d1b4c3d5da5d424cbe7f0
acuherb <acuherb@users.noreply.github.com>
2026-03-23 06:25:15 +0000

    deploy: acuherb/acuherb-blog@0a74bc226b9b8b4b136c201d55bb50332d81424a

--- js/link-preview.js
+++ js/link-preview.js
@@ -366,7 +366,7 @@ if (typeof module !== 'undefined' && module.exports) {
     },
     hideSelector: '',
     delay: 500,
-    hideDelay: 500,
+    hideDelay: 100,
     width: 550,
     height: 500
   });
